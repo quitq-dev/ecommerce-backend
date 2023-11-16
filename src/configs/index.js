b1d579c6c8cef0373259815249598cb14112924f
quitq-dev <quitq.dev@gmail.com>
2023-11-16 15:43:32 +0700

    Custom Dynamic Middleware for ApiKey and Permissions

--- src/configs/index.js
+++ src/configs/index.js
@@ -7,7 +7,13 @@ const ROLE_SHOP = {
   SHOP: "SHOP",
 };
 
+const HEADERS = {
+  API_KEY: "x-api-key",
+  AUTHORIZATION: "authorization",
+};
+
 module.exports = {
   STATUS,
   ROLE_SHOP,
+  HEADERS,
 };
