17f82153b063808114da98f4e4d9113f2d6df43b
quitq-dev <quitq.dev@gmail.com>
2023-11-16 15:43:32 +0700

    Custom Dynamic Middleware for ApiKey and Permissions

---
+++ src/auth/checkAuth.js
@@ -0,0 +1,39 @@
+const { HEADERS } = require("../configs");
+const findById = require("../services/apiKey.service");
+
+const apiKey = async (req, res, next) => {
+  try {
+    const key = req.headers[HEADERS.API_KEY];
+    if (!key) {
+      return res.status(403).json({
+        message: "Forbidden Error",
+      });
+    }
+    const objKey = await findById(key);
+    if (!objKey) {
+      return res.status(403).json({
+        code: 403,
+        message: "Forbidden Error",
+      });
+    }
+    req.objKey = objKey;
+    return next();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const permission = (permission) => {
+  return (req, res, next) => {
+    const { permissions } = req.objKey;
+    if (permissions && permissions.includes(permission)) {
+      return next();
+    }
+    return res.status(403).json({
+      code: 403,
+      message: "Permission Error",
+    });
+  };
+};
+
+module.exports = { apiKey, permission };
---
+++ src/services/apiKey.service.js
@@ -0,0 +1,14 @@
+const apiKeyModel = require("../models/api-key.model");
+const crypto = require("crypto");
+
+const findById = async (key) => {
+  // const newKey = await apiKeyModel.create({
+  //   key: crypto.randomBytes(64).toString("hex"),
+  //   permissions: ["0000"],
+  // }); 
+  // console.log("newKey", newKey);
+  const objKey = await apiKeyModel.findOne({ key }).lean();
+  return objKey;
+};
+
+module.exports = findById;
